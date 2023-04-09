28a14296be0a2963e6fff3c804cbd83b0d3a78a0
zeq886 <zeq886@outlook.com>
2023-04-09 22:07:56 +0800

    添加项目文件。

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            TodoItems = new ObservableCollection<string>();
+        }
+        [ObservableProperty]
+        string text;
+
+        [ObservableProperty]
+        ObservableCollection<string> todoItems;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+            TodoItems.Add(Text);
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Remove(string s)
+        {
+            if(TodoItems.Contains(s))
+                TodoItems.Remove(s);
+        }
+    }
+}
